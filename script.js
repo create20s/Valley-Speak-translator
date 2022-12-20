43bd92ad366ea083b2034d69748576604afe5131
create20s <86128319+create20s@users.noreply.github.com>
2022-12-20 16:42:52 +0530

    funtranslate api  updated

---
+++ script.js
@@ -0,0 +1,22 @@
+var textInput=document.querySelector("#textInput");
+var btnTranslate=document.querySelector("#btn-translate");
+var output=document.querySelector("#output");
+
+var serverURL = "https://api.funtranslations.com/translate/valspeak.json"
+
+function getTranslation(input){
+   return serverURL+ "?"+ "text="+ input;
+}
+
+
+function clickHandler(){
+var inputText=textInput.value;
+ fetch(getTranslation(inputText)).then(response=> response.json())
+ .then(json=>{
+   var translatedText= json.contents.translated;
+   output.innerText= translatedText;
+ }
+    )
+}
+btnTranslate.addEventListener("click",clickHandler);
+
